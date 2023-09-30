5551517864b4c5cf86ad361c5ef006b7fde73e70
Auridebson Sousa <auridebson@vutus.com>
2023-09-30 00:44:39 -0300

    Update

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # firebase-crud-operation
  
+
+
+ // criando o app no react
+npm creat-react-app
+
+// instalando o router-dom e toastify
+npm i --save react-router-dom firebase react-toastify
+
+
+  import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import './App.css';
+// https://www.youtube.com/watch?v=zEdI9L1MZU8
+
 import React from "react"
-import { BrowserRouter, Switch, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import './App.css';
 import Home from './pages/Home';
 import AddEdit from './pages/AddEdit';
 import About from './pages/About';
 import View from './pages/View';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Switch>
-        <Route exact path='/' Component={Home} />
-        <Route path='/add' Component={AddEdit} />
-        <Route path='/update/:id' Component={AddEdit} />
-        <Route path='/view/:id' Component={View} />
-        <Route path='/about' Component={About} />
-        </Switch>
+        <ToastContainer position="top-center">
+          <Routes>
+            <Route exact path='/home' Component={Home} />
+            <Route path='/add' Component={AddEdit} />
+            <Route path='/update/:id' Component={AddEdit} />
+            <Route path='/view/:id' Component={View} />
+            <Route path='/about' Component={About} />
+          </Routes>
+        </ToastContainer>
       </div>
     </BrowserRouter>
 
